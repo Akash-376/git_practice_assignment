2e305f2d6b95d3fbb23dbe26669c4fb0dc2d65c1
Rajesh <rajeshsonmurmur2017@gmail.com>
2022-09-15 09:29:54 +0530

    code for check prime number

--- check_prime.js
+++ check_prime.js
@@ -1,14 +1,14 @@
 
-let num = 41;
+let num = 31;
 
 let count = 0;
 
 for(let i=0;i<=num;i++){
-    if(num/i==0){
+    if(num%i==0){
         count++;
     }   
 }
-if(count==0){
+if(count==2){
     console.log("Prime");
 }else{
     console.log("Not Prime");
